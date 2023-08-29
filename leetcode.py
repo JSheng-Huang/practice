1fc859ffd8182d09bf867043e4adb9956a95364f
jsheng <jasonhuang0124@gmail.com>
2023-08-29 12:54:43 +0800

    230829 1254: Keep going~

--- leetcode.py
+++ leetcode.py
@@ -1,34 +1,32 @@
 class Solution:
-    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        # 12:06.
+    def bestClosingTime(self, customers: str) -> int:
+        # 11:37.
+    
+        n = len(customers)
+        y_cnt = 0
+        ans = 0
 
-        s1_len = len(s1)
-        s2_len = len(s2)
-        s3_len = len(s3)
-
-        if s1_len + s2_len != s3_len:
-            return False
-        else:
-            for i in range(s3_len):
-                s1_idx = 0
-                s2_idx = 0
-                print(s3[i])
-                if s1_idx < s1_len:
-                    if s1[s1_idx] == s3[i]:
-                        s1_idx += 1
-                    elif s2_idx < s2_len:
-                        if s2[s2_idx] == s3[i]:
-                            s2_idx += 1
-                        else:
-                            return False
-                elif s2_idx < s2_len:
-                    if s2[s2_idx] == s3[i]:
-                        s2_idx += 1
-                    else:
-                        return False
+        for i in customers:
+            if i == 'Y':
+                y_cnt += 1
         
-        return True
+        tmp = y_cnt
+
+        for i in range(n):
+            if customers[i] == 'Y':
+                y_cnt -= 1
+                penalty = y_cnt
+            else:
+                penalty = n - y_cnt - 1
+            print(tmp)
+            print(penalty)
+            print('===')
+            if tmp > penalty:
+                tmp = penalty
+                ans = i + 1
+        
+        return ans
 
 
 qwe = Solution()
-print(qwe.isInterleave("aabcc", "dbbca" , "aadbbcbcac"))
+print(qwe.bestClosingTime("YYNY"))
