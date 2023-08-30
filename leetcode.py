1ea9e55e50bf76d0068584a492a155cf8b852bab
jsheng <jasonhuang0124@gmail.com>
2023-08-30 12:54:41 +0800

    230830 1254: Keep going~

--- leetcode.py
+++ leetcode.py
@@ -1,32 +1,36 @@
+import heapq
+
 class Solution:
-    def bestClosingTime(self, customers: str) -> int:
-        # 11:37.
-    
-        n = len(customers)
-        y_cnt = 0
-        ans = 0
-
-        for i in customers:
-            if i == 'Y':
-                y_cnt += 1
+    def minCostConnectPoints(self, points) -> int:
+        N = len(points)
         
-        tmp = y_cnt
-
-        for i in range(n):
-            if customers[i] == 'Y':
-                y_cnt -= 1
-                penalty = y_cnt
-            else:
-                penalty = n - y_cnt - 1
-            print(tmp)
-            print(penalty)
+        adj = {i:[] for i in range(N)}
+        
+        for i in range(N):
+            x1, y1 = points[i]
+            for j in range(i+1, N):
+                x2, y2 = points[j]
+                dis = abs(x1-x2) + abs(y1-y2)
+                adj[i].append([dis, j])
+                adj[j].append([dis, i])
+        # print(adj)
+        res = 0
+        minHeap = [[0,0]]
+        visit = set()
+        while len(visit) < N:
+            cost, node = heapq.heappop(minHeap)
+            print([cost, node])
+            if node in visit:
+                continue
+            res += cost
+            for neighCost, nei in adj[node]:
+                if nei not in visit:
+                    heapq.heappush(minHeap, [neighCost, nei])
+                    print(minHeap)
             print('===')
-            if tmp > penalty:
-                tmp = penalty
-                ans = i + 1
-        
-        return ans
+            visit.add(node)
+        return res
 
 
 qwe = Solution()
-print(qwe.bestClosingTime("YYNY"))
+print(qwe.minCostConnectPoints([[0,0],[2,2],[3,10],[5,2],[7,0]]))
