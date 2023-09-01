09b0a0f06882a0d2dbde4200fc8d9aa92a2f1a96
jsheng <jasonhuang0124@gmail.com>
2023-09-01 08:32:14 +0800

    230901 0831: Keep going~

--- sort/quick_sort.py
+++ sort/quick_sort.py
@@ -1,6 +1,5 @@
-# Quick Sort(230828): https://ithelp.ithome.com.tw/articles/10202330
+# Quick Sort(230828): https://web.ntnu.edu.tw/~algo/Sort.html
 
-def quicksort(data, left, right): # 輸入資料，和從兩邊開始
 
 data = [89, 34, 23, 78, 67, 100, 66, 29, 79, 55, 78, 88, 92, 96, 96, 23]
 
