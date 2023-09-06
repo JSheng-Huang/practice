7738a29d44ce9e31e7c7f976387bbf824cfed614
jsheng <jasonhuang0124@gmail.com>
2023-09-06 13:30:44 +0800

    230906 1330: Keep going~

--- leetcode.py
+++ leetcode.py
@@ -1,28 +1,36 @@
 class Solution:
-    def buildArray(self, target, n: int):
-        # 12:07.
+    def checkValidString(self, s: str) -> bool:
+        # 11:38.
 
-        ans = ["Push"] * len(target)
-
-        pointer = 0
-
-        for i in range(n):
-            print('===')
-            print(ans)
-            if target[pointer] != i + 1:
-                print(i)
-                ans.insert(i, "Push")
-                ans.insert(i + 1, "Pop")
-            else:
-                pointer += 1
-            if pointer == len(target):
-                break
-
-        return ans          
+        if s[0] == ")":
+            return False
         
+        star_cnt = 0
+        check_list = []
+
+        for i in range(len(s)):
+            if s[i] == "*":
+                star_cnt += 1
+            else:
+                if s[i] == "(":
+                    check_list.append("(")
+                else:
+                    if len(check_list) == 0:
+                        if star_cnt == 0:
+                            return False
+                        else:
+                            star_cnt -= 1
+                    else:
+                        check_list.pop()
+        print(check_list)
+        print(star_cnt)
+        if len(check_list) <= star_cnt:
+            return True
+        else:
+            return False 
             
             
 
 
 qwe = Solution()
-print(qwe.buildArray([1,3,4,6,7,8], 9))
+print(qwe.checkValidString("(((((*(()((((*((**(((()()*)()()()*((((**)())*)*)))))))(())(()))())((*()()(((()((()*(())*(()**)()(())"))
