3d88ae21d9037ea049068afe7a0cb3835ad0d27b
jsheng <jasonhuang0124@gmail.com>
2023-09-13 12:52:12 +0800

    230913_1252: Keep going~

--- ds/linked_list.py
+++ ds/linked_list.py
@@ -10,6 +10,20 @@ class SingleLinkedList:
     def __init__(self) -> None:
         self.head = None
         self.tail = None
+        self.cnt = 0
+        return
+
+    def printLinkedList(self):
+        if self.head == None:
+            return print('You have to add nodes in the linked list first!')
+
+        node = self.head
+
+        while node:
+            arrow = ' => ' if node.next != None else '\n'
+            print(node.data, end=arrow)
+            node = node.next
+
         return
 
     def add(self, node):
@@ -22,14 +36,45 @@ class SingleLinkedList:
             self.tail.next = node
 
         self.tail = node
+        self.cnt += 1
+
         return
 
+    def insert(self, idx, data):
+        if idx == self.cnt:
+            self.add(data)
+        elif idx > self.cnt:
+            return print("Out of bound!")
+        else:
+            tmp = 0
+            node = self.head
 
-linked_list = SingleLinkedList()
+            while tmp != idx:
+                node = node.next
+                tmp += 1
 
-node1 = ListNode(0)
+            # change?
+            self.add(data)
 
-linked_list.add(node1)
-linked_list.add(1)
 
-print(linked_list.tail.data)
+if __name__ == '__main__':
+    linked_list = SingleLinkedList()
+
+    node1 = ListNode(0)
+
+    linked_list.printLinkedList()
+
+    linked_list.add(node1)
+    linked_list.printLinkedList()
+
+    linked_list.add(1)
+    linked_list.add(2)
+    linked_list.add(3)
+    linked_list.printLinkedList()
+
+    linked_list.insert(4, 4)
+    linked_list.printLinkedList()
+    linked_list.insert(10000, 5)
+    linked_list.printLinkedList()
+    linked_list.insert(1, -1)
+    linked_list.printLinkedList()
