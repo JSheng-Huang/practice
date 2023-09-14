0ea3c358c582c03e13936d3c99b10d6e1cee4edd
jsheng <jasonhuang0124@gmail.com>
2023-09-14 08:43:55 +0800

    230914_0843: Keep going~

--- sort/quick_sort.py
+++ sort/quick_sort.py
@@ -1,13 +1,15 @@
 # Quick Sort(230828): https://www.geeksforgeeks.org/quick-sort/
 
 # data = [89, 34, 23, 78, 67, 100, 66, 29, 79, 55, 78, 88, 92, 96, 96, 23]
-data = [10, 80, 30, 90, 40]
+data = [10, 80, 70, 20, 30, 90, 40]
 
 # Function to find the partition position
 
 
 def partition(array, low, high):
     print('Partitioning: ', data)
+    print('Low: ', low)
+    print('High: ', high)
     # choose the rightmost element as pivot
     pivot = array[high]
 
