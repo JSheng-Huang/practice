809f2046763e4dd690e88a530bd0a164618ab6c1
jsheng <jasonhuang0124@gmail.com>
2023-09-18 08:48:52 +0800

    230918_0848: Keep going~

--- sort/quick_sort copy.py
+++ sort/quick_sort copy.py
@@ -1,19 +1,36 @@
 # Quick Sort(230828): https://www.geeksforgeeks.org/quick-sort/
 
-data = [89, 34, 23, 78, 67, 100, 66, 29, 79, 55, 78, 88, 92, 96, 96, 23]
-# data = [10, 80, 70, 20, 30, 90, 40]
+# data = [89, 34, 23, 78, 67, 100, 66, 29, 79, 55, 78, 88, 92, 96, 96, 23]
+data = [10, 80, 70, 20, 30, 90, 40]
 
 # Function to find the partition position
 
 
 def partition(array, low, high):
-    pass
+    i = low - 1
+    pivot = array[high]
 
-# function to perform quicksort
+    for j in range(low, high):
+        # Why?
+        if array[j] < pivot:
+            i += 1
+            array[i], array[j] = array[j], array[i]
+
+    # Why?
+    # array[i], array[j] = array[j], array[i]
+    (array[i + 1], array[high]) = (array[high], array[i + 1])
+    print(array)
+    return i + 1
+
+# Function to perform quicksort
 
 
 def quickSort(array, low, high):
-    pass
+    if low < high:
+        pos = partition(array, low, high)
+
+        quickSort(array, pos + 1, high)
+        quickSort(array, low, pos - 1)
 
 
 if __name__ == '__main__':
