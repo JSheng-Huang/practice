7f5915a72b96f4dc139d1dd9993673169b1030ae
jsheng <jasonhuang0124@gmail.com>
2023-09-21 12:57:14 +0800

    230921_1257: Keep going!

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# practice_Git_command
+# Practice
 As title.
--- ds/rat_in_maze_backtracking.py
+++ ds/rat_in_maze_backtracking.py
@@ -1,9 +1,6 @@
 # https://www.codingninjas.com/studio/online-compiler/online-python-compiler
 
-N = 4
-
-
-def checkposition(m, x, y):
+def isValid(m, x, y):
     if x >= 0 and x < len(m):
         if y >= 0 and y < len(m[0]):
             if m[x][y] == 1:
@@ -12,66 +9,60 @@ def checkposition(m, x, y):
     return False
 
 
-def solvemaze(m, sol):
-
-    if rat_maze_sol(m, 0, 0, sol) == False:
-        return False
-
-    for i in sol:
-        for j in i:
-            print(str(j) + " ", end="")
-        print("")
-    return True
-
-
-def rat_maze_sol(m, x, y, sol):
-    print('in')
+def solveMaze(m, x, y, s):
     if x == len(m) - 1 and y == len(m[0]) - 1 and m[x][y] == 1:
-        sol[x][y] = 1
+        s[x][y] = 1
         return True
 
-    if checkposition(m, x, y) == True:
-        if sol[x][y] == 1:
+    if isValid(m, x, y):
+        if s[x][y] == 1:
             return False
 
-        sol[x][y] = 1
+        s[x][y] = 1
 
-        if rat_maze_sol(m, x + 1, y, sol):
+        if solveMaze(m, x + 1, y, s):
             return True
 
-        if rat_maze_sol(m, x, y + 1, sol) == True:
+        if solveMaze(m, x, y + 1, s):
             return True
 
-        if rat_maze_sol(m, x - 1, y, sol) == True:
+        if solveMaze(m, x - 1, y, s):
             return True
 
-        if rat_maze_sol(m, x, y - 1, sol) == True:
+        if solveMaze(m, x, y - 1, s):
             return True
 
-        sol[x][y] = 0
+        s[x][y] = 0
         return False
 
 
-m = [
-    [1, 0, 0, 0],
-    [1, 1, 0, 1],
-    [0, 1, 0, 0],
-    [1, 1, 1, 1]
-]
+if __name__ == '__main__':
+    m = [
+        [1, 0, 0, 1],
+        [1, 1, 0, 1],
+        [1, 1, 1, 1],
+        [0, 0, 0, 1]
+    ]
 
-sol = [
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0]
-]
-# Different! https://skylinelimit.blogspot.com/2018/04/python-variable-reference.html
-solu = [[0] * len(m)] * len(m[0])
-Lt = [[0] * 3 for i in range(3)]
+    # Both are available.
+    s = [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+    ]
+    s = [[0] * len(m) for i in range(len(m[0]))]
+    # This would fail because of shallow copy:
+    # 1. https://skylinelimit.blogspot.com/2018/04/python-variable-reference.html
+    # 2. https://ithelp.ithome.com.tw/articles/10221255
+    # s = [[0] * len(m)] * len(m[0])
 
-print(type(sol))
-print(type(solu))
-# solvemaze(m, sol)
-rat_maze_sol(m, 0, 0, sol)
-print('---')
-rat_maze_sol(m, 0, 0, solu)
+    try:
+        if solveMaze(m, 0, 0, s):
+            print('INTC Yes!')
+        else:
+            print('AMD Yes!')
+    except:
+        print('Infinite Loop!')
+    finally:
+        print(s)
