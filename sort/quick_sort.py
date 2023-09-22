f5513daa0b559c7ee388d426f9fd9583975e650e
jsheng <jasonhuang0124@gmail.com>
2023-09-22 09:01:53 +0800

    230922_0901: Keep going!

--- sort/quick_sort.py
+++ sort/quick_sort.py
@@ -3,20 +3,25 @@
 data = [89, 34, 23, 78, 67, 100, 66, 29, 79, 55, 78, 88, 92, 96, 96, 23]
 # data = [10, 80, 70, 20, 30, 90, 40]
 
-# Function to find the partition position
-
 
 def partition(array, low, high):
     i = low - 1
     pivot = array[high]
 
     for j in range(low, high):
+        # If `array[j]` is less than `pivot`,
+        # swap it with `array[i]`, which is the element
+        # that pointed by `i`.
         if array[j] < pivot:
             i += 1
             array[i], array[j] = array[j], array[i]
 
+    # Swap `array[high]` which is the pivot with `array[i]`,
+    # which is the element that pointed by `i`.
     array[i + 1], array[high] = array[high], array[i + 1]
 
+    # Return the position from where partition is done,
+    # it means `array[i]` is at the right position.
     return i + 1
 
 
