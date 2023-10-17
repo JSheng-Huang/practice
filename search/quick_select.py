a6002443d95e50bab43b82ebac0948b61f38027d
jsheng <jasonhuang0124@gmail.com>
2023-10-17 08:51:03 +0800

    231017_0851: Keep going!

--- search/quick_select.py
+++ search/quick_select.py
@@ -3,6 +3,8 @@
 # example is wrong.
 # https://www.geeksforgeeks.org/quickselect-algorithm/
 #
+# ASSUMPTION: All elements in arr[] are distinct.
+#
 # Created by JSheng <jasonhuang0124@gmail.com>
 #
 
@@ -45,44 +47,32 @@ def partition(arr, left_idx, right_idx):
 
     return idx
 
-# finds the kth position (of the sorted array)
-# in a given unsorted array i.e this function
-# can be used to find both kth largest and
-# kth smallest element in the array.
-# ASSUMPTION: all elements in arr[] are distinct
+
+def kth_smallest(arr, left_idx, right_idx, k):
+    # # If k is smaller than number of
+    # # elements in array.
+    if (k > 0 and k <= right_idx - left_idx + 1):
+        # # Partition the array around last
+        # # element and get position of pivot
+        # # element in sorted array.
+        pivot_idx = partition(arr, left_idx, right_idx)
+
+        # # If position is same as input `k.`
+        if (pivot_idx - left_idx == k - 1):
+            return arr[pivot_idx]
+        # # If position is more, recur
+        # # for left subarray.
+        if (pivot_idx - left_idx > k - 1):
+            return kth_smallest(arr, left_idx, pivot_idx - 1, k)
+        # # Else recur for right subarray.
+        # # ?????
+        return kth_smallest(arr, pivot_idx + 1, right_idx,
+                            k - pivot_idx + left_idx - 1)
+    print('[ERROR] Index out of bound!')
 
 
-def kthSmallest(arr, l, r, k):
-
-    # if k is smaller than number of
-    # elements in array
-    if (k > 0 and k <= r - l + 1):
-
-        # Partition the array around last
-        # element and get position of pivot
-        # element in sorted array
-        index = partition(arr, l, r)
-
-        # if position is same as k
-        if (index - l == k - 1):
-            return arr[index]
-
-        # If position is more, recur
-        # for left subarray
-        if (index - l > k - 1):
-            return kthSmallest(arr, l, index - 1, k)
-
-        # Else recur for right subarray
-        return kthSmallest(arr, index + 1, r,
-                           k - index + l - 1)
-    print("Index out of bound")
-
-
-# Driver Code
 arr = [47, 32, 30, 100, 38, 9, 101, 2, 61, 69, 81, 79]
 n = len(arr)
 k = 2
 print("K-th smallest element is ")
-print(kthSmallest(arr, 0, n - 1, k))
-
-# This code is contributed by Muskan Kalra.
+print(kth_smallest(arr, 0, n - 1, k))
