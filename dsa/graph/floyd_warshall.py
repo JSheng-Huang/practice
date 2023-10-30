013650529b5d1abf62c4e3ad01c0fcc5972af616
jsheng <jasonhuang0124@gmail.com>
2023-10-30 12:57:47 +0800

    231030_1257: Keep going!

--- dsa/graph/floyd_warshall.py
+++ dsa/graph/floyd_warshall.py
@@ -5,40 +5,40 @@
 Created by JSheng <jasonhuang0124@gmail.com>"""
 
 
+"""
+# # Consider all vertices one by one should be added into the shortest path 
+# # from A to B or not.
+"""
+
+
 def floyd_warshall(graph):
     dist = list(map(lambda i: list(map(lambda j: j, i)), graph))
     graph_len = len(dist)
-    """ Add all vertices one by one 
+
+    """ 
+    Add all vertices one by one 
 	to the set of intermediate
 	vertices.
-	---> Before start of an iteration, 
-	we have shortest distances
-	between all pairs of vertices 
-	such that the shortest
-	distances consider only the 
-	vertices in the set 
-	{0, 1, 2, .. k-1} as intermediate vertices.
-	----> After the end of a 
-	iteration, vertex no. k is
-	added to the set of intermediate 
-	vertices and the 
-	set becomes {0, 1, 2, .. k}
 	"""
     for k in range(graph_len):
-
-        # pick all vertices as source one by one
+        """
+        # # Pick all vertices as source one by one.
+        """
         for i in range(graph_len):
-
-            # Pick all vertices as destination for the
-            # above picked source
+            """
+            # # Pick all vertices as destination for the
+            # # above picked source.
+            """
             for j in range(graph_len):
-
-                # If vertex k is on the shortest path from
-                # i to j, then update the value of dist[i][j]
+                """
+                # # If vertex k is on the shortest path from
+                # # `i` to `j`, then update the value of `dist[i][j]`.
+                """
                 dist[i][j] = min(dist[i][j],
                                  dist[i][k] + dist[k][j]
                                  )
-    print(dist)
+    for i in range(graph_len):
+        print(dist[i])
 
 
 if __name__ == "__main__":
