2229950739c462f542893290c86d503ae01c5fcc
jsheng <jasonhuang0124@gmail.com>
2023-10-31 09:00:15 +0800

    231031_0900: Keep going!

--- leetcode/binary_search/4_hard_median_of_two_sorted_arrays.py
+++ leetcode/binary_search/4_hard_median_of_two_sorted_arrays.py
@@ -1,10 +1,13 @@
 """LeetCode#4(Hard) Median of Two Sorted Arrays
 # # Problem:
 # # There are two sorted arrays nums1 and nums2 of size m and n respectively.
-# # Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+# # Find the median of the two sorted arrays. The overall run time complexity should be O(log(m + n)).
 # # 1. https://zxi.mytechroad.com/blog/algorithms/binary-search/leetcode-4-median-of-two-sorted-arrays/
+# # Time Complexity: O(log(min(n1, n2)))
+# # Space Complexity: O(1)
 # # 2. https://github.com/wisdompeak/LeetCode/tree/master/Priority_Queue/004.Median-of-Two-Sorted-Arrays
 # # Time Complexity: ???
+# # Space Complexity: ???
 Created by JSheng <jasonhuang0124@gmail.com>"""
 
 
@@ -12,98 +15,91 @@ class Solution:
     def findMedianSortedArrays(self, nums1, nums2):
         nums1_len = len(nums1)
         nums2_len = len(nums2)
-        if (nums1_len + nums2_len) % 2 == 1:
-            return self.findKthSmallest(nums1, 0, nums1_len, nums2, 0, nums2_len, (nums1_len + nums2_len + 1) // 2)
-        else:
-            return (self.findKthSmallest(nums1, 0, nums1_len, nums2, 0, nums2_len, (nums1_len + nums2_len) // 2) + self.findKthSmallest(nums1, 0, nums1_len, nums2, 0, nums2_len, (nums1_len + nums2_len) // 2 + 1)) / 2
 
-    def findKthSmallest(self, nums1, nums1_bgn, nums1_len, nums2, nums2_bgn, nums2_len, k):
         """
-        # # Always find in the smaller one.
+        # # Make sure `nums1` length <= `nums2` length for the following 
+        # # operation.
         """
         if nums1_len > nums2_len:
-            return self.findKthSmallest(nums2, nums2_bgn, nums2_len, nums1, nums1_bgn, nums1_len, k)
+            return self.findMedianSortedArrays(nums2, nums1)
+        target = int((nums1_len + nums2_len + 1) / 2)
+        l = 0
+        r = nums1_len
+
         """
-        # # Because the answer is not in `nums1`,
-        # # just find it in `nums2`.
+        # # ???
         """
-        if nums1_len == 0:
-            return nums2[nums2_bgn + k - 1]
-        """
-        # # `k == 1` => Find the smallest one in two lists.
-        """
-        if k == 1:
-            return min(nums1[nums1_bgn], nums2[nums2_bgn])
-        """
-        # # Example:
-        # # [2, 4, 6]
-        # # [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
-        # # `k` == 7
-        # # `k_1` == 3
-        # # `k_2` == 4
-        # # `nums1[int(nums1_bgn + k_1 - 1)]` == 6
-        # # `nums2[int(nums2_bgn + k_2 - 1)]` == 5
-        # # Compare the (k/2)-th smallest number between `nums1` and `nums2`,
-        # # if the (k/2)-th smallest number in `nums1` is smaller than the one
-        # # in `nums2`, then the k-th smallest number would not be in the top 
-        # # k/2 numbers in `nums1`, because the k-th smallest number is larger 
-        # # than the (k/2)-th smallest number, and vice versa.
-        # # The maximum of `k_1` is k/2, because we always choose the smaller
-        # # list as `nums1`.
-        """
-        k_1 = min(nums1_len, k // 2)
-        k_2 = k - k_1
-        if nums1[int(nums1_bgn + k_1 - 1)] < nums2[int(nums2_bgn + k_2 - 1)]:
-            return self.findKthSmallest(nums1, nums1_bgn + k_1, nums1_len - k_1, nums2, nums2_bgn, nums2_len, k - k_1)
-        else:
-            return self.findKthSmallest(nums1, nums1_bgn, nums1_len, nums2, nums2_bgn + k_2, nums2_len - k_2, k - k_2)
+        while l < r:
+            m1 = l + (r - l) // 2
+            m2 = target - m1
+            if nums1[m1] < nums2[m2 - 1]:
+                l = m1 + 1
+            else:
+                r = m1
+        m1 = l
+        m2 = target - l
+        print('m1: ', m1)
+        print('m2: ', m2)
+        # ans1 = max(float('-inf') if m1 <=
+        #            0 else nums1[m1 - 1], float('-inf') if m2 <= 0 else nums2[m2 - 1])
+        ans1 = max(nums1[m1 - 1], nums2[m2 - 1])
+        if (nums1_len + nums2_len) % 2 == 1:
+            return ans1
+        # ans2 = min(float('inf') if m1 >= nums1_len else nums1[m1], float(
+        #     'inf') if m2 >= nums2_len else nums2[m2])
+        ans2 = min(nums1[m1], nums2[m2])
+        return (ans1 + ans2) / 2
 
 
-"""
-# # Refer to: https://github.com/wisdompeak/LeetCode/tree/master/Priority_Queue/004.Median-of-Two-Sorted-Arrays
-"""
+# class Solution:
+#     def findMedianSortedArrays(self, nums1, nums2):
+#         nums1_len = len(nums1)
+#         nums2_len = len(nums2)
+#         if (nums1_len + nums2_len) % 2 == 1:
+#             return self.findKthSmallest(nums1, 0, nums1_len, nums2, 0, nums2_len, (nums1_len + nums2_len + 1) // 2)
+#         else:
+#             return (self.findKthSmallest(nums1, 0, nums1_len, nums2, 0, nums2_len, (nums1_len + nums2_len) // 2) + self.findKthSmallest(nums1, 0, nums1_len, nums2, 0, nums2_len, (nums1_len + nums2_len) // 2 + 1)) / 2
 
-
-class Solution:
-    def findMedianSortedArrays(self, nums1, nums2):
-        pass
-# class Solution {
-# public:
-#     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-#         const int n1 = nums1.size();
-#         const int n2 = nums2.size();
-#         // Make sure n1 <= n2
-#         if (n1 > n2) return findMedianSortedArrays(nums2, nums1);
-
-#         const int k = (n1 + n2 + 1) / 2;
-
-#         int l = 0;
-#         int r = n1;
-
-#         while (l < r) {
-#             const int m1 = l + (r - l) / 2;
-#             const int m2 = k - m1;
-#             if (nums1[m1] < nums2[m2 - 1])
-#                 l = m1 + 1;
-#             else
-#                 r = m1;
-#         }
-
-#         const int m1 = l;
-#         const int m2 = k - l;
-
-#         const int c1 = max(m1 <= 0 ? INT_MIN : nums1[m1 - 1],
-#                            m2 <= 0 ? INT_MIN : nums2[m2 - 1]);
-
-#         if ((n1 + n2) % 2 == 1)
-#             return c1;
-
-#         const int c2 = min(m1 >= n1 ? INT_MAX : nums1[m1],
-#                            m2 >= n2 ? INT_MAX : nums2[m2]);
-
-#         return (c1 + c2) * 0.5;
-#     }
-# };
+#     def findKthSmallest(self, nums1, nums1_bgn, nums1_len, nums2, nums2_bgn, nums2_len, k):
+#         """
+#         # # Always find in the smaller one.
+#         """
+#         if nums1_len > nums2_len:
+#             return self.findKthSmallest(nums2, nums2_bgn, nums2_len, nums1, nums1_bgn, nums1_len, k)
+#         """
+#         # # Because the answer is not in `nums1`,
+#         # # just find it in `nums2`.
+#         """
+#         if nums1_len == 0:
+#             return nums2[nums2_bgn + k - 1]
+#         """
+#         # # `k == 1` => Find the smallest one in two lists.
+#         """
+#         if k == 1:
+#             return min(nums1[nums1_bgn], nums2[nums2_bgn])
+#         """
+#         # # Example:
+#         # # [2, 4, 6]
+#         # # [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+#         # # `k` == 7
+#         # # `k_1` == 3
+#         # # `k_2` == 4
+#         # # `nums1[int(nums1_bgn + k_1 - 1)]` == 6
+#         # # `nums2[int(nums2_bgn + k_2 - 1)]` == 5
+#         # # Compare the (k/2)-th smallest number between `nums1` and `nums2`,
+#         # # if the (k/2)-th smallest number in `nums1` is smaller than the one
+#         # # in `nums2`, then the k-th smallest number would not be in the top
+#         # # k/2 numbers in `nums1`, because the k-th smallest number is larger
+#         # # than the (k/2)-th smallest number, and vice versa.
+#         # # The maximum of `k_1` is k/2, because we always choose the smaller
+#         # # list as `nums1`.
+#         """
+#         k_1 = min(nums1_len, k // 2)
+#         k_2 = k - k_1
+#         if nums1[int(nums1_bgn + k_1 - 1)] < nums2[int(nums2_bgn + k_2 - 1)]:
+#             return self.findKthSmallest(nums1, nums1_bgn + k_1, nums1_len - k_1, nums2, nums2_bgn, nums2_len, k - k_1)
+#         else:
+#             return self.findKthSmallest(nums1, nums1_bgn, nums1_len, nums2, nums2_bgn + k_2, nums2_len - k_2, k - k_2)
 
 
 if __name__ == '__main__':
