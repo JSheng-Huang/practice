c2e0d7ba7bcc3d29af427e7f51aedbec70f35558
jsheng <jasonhuang0124@gmail.com>
2023-11-02 12:53:05 +0800

    231102_1252: Keep going!

--- dsa/graph/kruskals.py
+++ dsa/graph/kruskals.py
@@ -15,21 +15,15 @@ class Graph:
     def add_edge(self, bgn, end, weight):
         self.graph.append([bgn, end, weight])
 
-    # A utility function to find set of an element i
-    # (truly uses path compression technique)
     def find(self, parent, i):
+        """Find the root vertex."""
         if parent[i] != i:
-
-            # Reassignment of node's parent
-            # to root node as
-            # path compression requires
             parent[i] = self.find(parent, parent[i])
-        # print(parent)
-        # print(parent[i])
         return parent[i]
 
     # A function that does union of two sets of x and y
     # (uses union by rank)
+
     def union(self, parent, rank, x, y):
 
         # Attach smaller rank tree under root of
@@ -45,12 +39,8 @@ class Graph:
             parent[y] = x
             rank[x] += 1
 
-    # The main function to construct MST
-    # using Kruskal's algorithm
     def Kruskal_MST(self):
-
-        # This will store the resultant MST
-        result = []
+        ans = []
 
         # An index variable, used for sorted edges
         i = 0
@@ -71,8 +61,6 @@ class Graph:
         for node in range(self.vertex):
             parent.append(node)
             rank.append(0)
-        print(parent)
-        print(rank)
         # Number of edges to be taken is less than to V-1
         while e < self.vertex - 1:
 
@@ -80,6 +68,11 @@ class Graph:
             # the index for next iteration
             u, v, w = self.graph[i]
             i = i + 1
+            print(e)
+            """
+            In the first loop, they are meaningless, because all vertices are 
+            unconnected in the beginning.
+            """
             x = self.find(parent, u)
             y = self.find(parent, v)
 
@@ -89,14 +82,15 @@ class Graph:
             # for next edge
             if x != y:
                 e = e + 1
-                result.append([u, v, w])
+                ans.append([u, v, w])
+                print('parent before union(): ', parent)
                 self.union(parent, rank, x, y)
-                print(parent)
+                print('parent after union(): ', parent)
             # Else discard the edge
 
         minimumCost = 0
         print('Edges in the constructed MST')
-        for u, v, weight in result:
+        for u, v, weight in ans:
             minimumCost += weight
             print('%d -- %d == %d' % (u, v, weight))
         print('Minimum Spanning Tree', minimumCost)
