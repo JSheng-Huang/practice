5bbc07d785f941dd7a57d0510a6bd53eca6dada3
jsheng <jasonhuang0124@gmail.com>
2023-11-08 09:00:17 +0800

    231108_0900: Keep going!

--- dsa/linked_list/linked_list.py
+++ dsa/linked_list/linked_list.py
@@ -1,8 +1,7 @@
-"""
-Refer to: https://p61402.github.io/2017/09/02/%E9%80%A3%E7%B5%90%E4%B8%B2%E5%88%97-Linked-List/
-#
-# Created by JSheng <jasonhuang0124@gmail.com>
-"""
+"""Linked List
+Refer to: 
+    1. https://p61402.github.io/2017/09/02/%E9%80%A3%E7%B5%90%E4%B8%B2%E5%88%97-Linked-List/
+Created by JSheng <jasonhuang0124@gmail.com>"""
 
 
 class ListNode:
@@ -29,43 +28,68 @@ class SingleLinkedList:
         return
 
     def add(self, node):
-        # # Assert input is a node.
+        """Assert input is a node."""
         if not isinstance(node, ListNode):
             node = ListNode(node)
-
-        # # If linked list is empty,
+        """If linked list is empty, or append the new node to tail."""
         if self.head == None:
             self.head = node
-        # # or append the new node to tail.
         else:
             self.tail.next = node
-        # # Tail points to the new mode.
+        """Tail points to the new mode."""
         self.tail = node
 
         self.cnt += 1
         return
 
     def insert(self, idx, data):
-        # # (jason_huang): If the location is the end of linked list, it means
-        # # appended.
         if idx == self.cnt:
             self.add(data)
         elif idx > self.cnt:
-            return print("Out of bound!")
-        # # In bound and not the end.
+            return print('[ERROR] OUT OF BOUND!')
         else:
             node = self.head
-            inserted_node = ListNode(data)
+            insertedNode = ListNode(data)
             if idx == 0:
-                inserted_node.next = node
-                self.head = inserted_node
+                insertedNode.next = node
+                self.head = insertedNode
             else:
                 tmp = 0
                 while tmp != idx - 1:
                     node = node.next
                     tmp += 1
-                inserted_node.next = node.next
-                node.next = inserted_node
+                insertedNode.next = node.next
+                node.next = insertedNode
+
+    def insertSorted(self, data):
+        if not isinstance(node, ListNode):
+            node = ListNode(data)
+        if self.head == None:
+            self.head = node
+        elif self.head.data > data:
+            self.insert(0, data)
+        else:
+            curNode = self.head
+            while curNode.next is not None and curNode.next.data < node.data:
+                curNode = curNode.next
+
+        # if idx == self.cnt:
+        #     self.add(data)
+        # elif idx > self.cnt:
+        #     return print("Out of bound!")
+        # else:
+        #     node = self.head
+        #     insertedNode = ListNode(data)
+        #     if idx == 0:
+        #         insertedNode.next = node
+        #         self.head = insertedNode
+        #     else:
+        #         tmp = 0
+        #         while tmp != idx - 1:
+        #             node = node.next
+        #             tmp += 1
+        #         insertedNode.next = node.next
+        #         node.next = insertedNode
 
     def delete(self, data):
         prev = None
@@ -84,26 +108,27 @@ class SingleLinkedList:
 
 
 if __name__ == '__main__':
-    linked_list = SingleLinkedList()
-    node1 = ListNode(0)
-    linked_list.printLinkedList()
-    linked_list.add(node1)
-    linked_list.printLinkedList()
-    linked_list.add(1)
-    linked_list.add(2)
-    linked_list.add(3)
-    linked_list.printLinkedList()
-    linked_list.insert(4, 4)
-    linked_list.printLinkedList()
-    linked_list.insert(10000, 5)
-    linked_list.printLinkedList()
-    linked_list.insert(0, -1)
-    linked_list.printLinkedList()
-    linked_list.insert(0, -1)
-    linked_list.printLinkedList()
-    linked_list.insert(1, -10)
-    linked_list.printLinkedList()
-    linked_list.insert(2, -20)
-    linked_list.printLinkedList()
-    linked_list.delete(-1)
-    linked_list.printLinkedList()
+    # linked_list = SingleLinkedList()
+    # node1 = ListNode(0)
+    # linked_list.printLinkedList()
+    # linked_list.add(node1)
+    # linked_list.printLinkedList()
+    # linked_list.add(1)
+    # linked_list.add(2)
+    # linked_list.add(3)
+    # linked_list.printLinkedList()
+    # linked_list.insert(4, 4)
+    # linked_list.printLinkedList()
+    # linked_list.insert(10000, 5)
+    # linked_list.printLinkedList()
+    # linked_list.insert(0, -1)
+    # linked_list.printLinkedList()
+    # linked_list.insert(0, -1)
+    # linked_list.printLinkedList()
+    # linked_list.insert(1, -10)
+    # linked_list.printLinkedList()
+    # linked_list.insert(2, -20)
+    # linked_list.printLinkedList()
+    # linked_list.delete(-1)
+    # linked_list.printLinkedList()
+    pass
