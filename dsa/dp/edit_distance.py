da16f8897794abd90c1ad05e035e45e58d4548cd
jsheng <jasonhuang0124@gmail.com>
2023-11-14 12:47:38 +0800

    231114_1247: Keep going!

--- dsa/dp/edit_distance.py
+++ dsa/dp/edit_distance.py
@@ -6,14 +6,17 @@ Problem:
     Operation 1(insert): Insert any character before or after any index of 
     `str1`.
     Operation 2(remove): Remove a character of `str1.`
-    Operation 3(replace): Replace a character at any index of `str1` with some other character.
+    Operation 3(replace): Replace a character at any index of `str1` with some 
+    other character.
 Example:
     Input: `str1 = 'sunday'`, `str2 = 'saturday'`.
     Output: `3`
-    Explanation: Last three and first characters are same. We basically need to convert "un" to "atur".  This can be done using below three operations. Replace "n" with "r", insert "t", insert "a".
+    Explanation: Last three and first characters are same. We basically need to 
+    convert "un" to "atur".  This can be done using below three operations. 
+    Replace "n" with "r", insert "t", insert "a".
 Refer to:
     1. https://www.geeksforgeeks.org/edit-distance-dp-5/
-    Time Complexity: O(m x n) where `m` and `n` are lengths of `str1` and 
+    Time Complexity: O(m x n), where `m` and `n` are lengths of `str1` and 
     `str2`.
     Space Complexity: O(n), where `n` is length of `str2`.
 Created by JSheng <jasonhuang0124@gmail.com>"""
@@ -22,45 +25,40 @@ Created by JSheng <jasonhuang0124@gmail.com>"""
 def editDistance(str1, str2):
     str1_len = len(str1)
     str2_len = len(str2)
-
-    # Initialize a list to store the current row
-    curr = [0] * (str2_len + 1)
-
-    # Initialize the first row with values from 0 to n
-    for j in range(str2_len + 1):
-        curr[j] = j
-
-    # Initialize a variable to store the previous value
-    previous = 0
+    curr = [i for i in range(str2_len + 1)]
+    prev = 0
 
     # Loop through the rows of the dynamic programming matrix
     for i in range(1, str1_len + 1):
-        # Store the current value at the beginning of the row
-        previous = curr[0]
+        prev = curr[0]
         curr[0] = i
-
-        # Loop through the columns of the dynamic programming matrix
         for j in range(1, str2_len + 1):
-            # Store the current value in a temporary variable
             temp = curr[j]
-
-            # Check if the characters at the current positions in str1 and str2 are the same
+            if i == 3 and j == 2:
+                print(curr)
+                print('temp', temp)
+            if i == 3 and j == 3:
+                print('before', curr)
+                print('current j:', j)
+                print('prev: ', prev)
             if str1[i - 1] == str2[j - 1]:
-                curr[j] = previous
+                curr[j] = prev
             else:
-                # Update the current cell with the minimum of the three adjacent cells
-                curr[j] = 1 + min(previous, curr[j - 1], curr[j])
-
-            # Update the previous variable with the temporary value
-            previous = temp
-
-    # The value in the last cell represents the minimum number of operations
+                curr[j] = 1 + min(prev, curr[j - 1], curr[j])
+            prev = temp
+            if i == 3 and j == 3:
+                print('in:', curr)
+                print('===')
+    """
+    Values in `curr` mean if `str1` is only considered from `0` to
+    ???
+    """
     return curr[str2_len]
 
 
 if __name__ == "__main__":
-    str1 = 'sit'
-    str2 = 'kiit'
+    str1 = 'hit'
+    str2 = 'kitty'
 
-    """The answer is 2 when `str1 = 'sit'` and `str2 = 'kiit'`."""
+    """The answer is 3 when `str1 = 'hit'` and `str2 = 'kitty'`."""
     print('Edit distance is:', editDistance(str1, str2))
