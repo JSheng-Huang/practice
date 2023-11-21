c2d53bc1862871aa23920e59830f6d9f3d6b13e6
jsheng <jasonhuang0124@gmail.com>
2023-11-21 12:50:39 +0800

    231121_1250: Keep going!

--- dsa/dp/optimal_strategy_for_a_game.py
+++ dsa/dp/optimal_strategy_for_a_game.py
@@ -23,24 +23,62 @@ def optimalStrategyOfGame(arr, n):
     # from diagonal elements to
     # table[0][n-1] which is the result.
     for gap in range(n):
+        print('gap', gap)
         for j in range(gap, n):
+            print('j', j)
             i = j - gap
+            print('i', i)
+            # print('i', i)
+            # print('j', j)
 
             # Here x is value of F(i + 2, j),
             # y is F(i + 1, j-1) and z is
             # F(i, j-2) in above recursive
             # formula
-            x = 0
-            if ((i + 2) <= j):
+            """
+            Example:
+                [8, 15, 11, 22]
+                [0, 15, 15, 18]
+                [0,  0,  3,  7]
+                [0,  0,  0,  7]
+            If `gap` == 0: Only one option.
+            If `gap` == 1: Two options, so pick the larger one.
+            e.g.:
+                `j` == 1: [ 8, 15], pick `15`.
+                `j` == 2: [15,  3], pick `15`.
+                `j` == 3: [ 3,  7], pick `7`.
+            """
+            if gap < 2:
+                x = 0
+                y = 0
+                z = 0
+            else:
                 x = table[i + 2][j]
-            y = 0
-            if ((i + 1) <= (j - 1)):
                 y = table[i + 1][j - 1]
-            z = 0
-            if (i <= (j - 2)):
                 z = table[i][j - 2]
-            table[i][j] = max(arr[i] + min(x, y),
-                              arr[j] + min(y, z))
+
+        #     x = 0
+        #     if ((i + 2) <= j):
+        #         x = table[i + 2][j]
+        #         print('gap', gap)
+        #         print('j', j)
+        #         print('i', i)
+        #         print('x')
+        #     y = 0
+        #     if ((i + 1) <= (j - 1)):
+        #         y = table[i + 1][j - 1]
+        #         print('y')
+        #     z = 0
+        #     if (i <= (j - 2)):
+        #         z = table[i][j - 2]
+        #         print('z')
+            table[i][j] = max(arr[i] + min(x, y), arr[j] + min(y, z))
+        #     print('---')
+        # print('===')
+            for k in range(4):
+                print(table[k])
+            print('---')
+        print('===')
     return table[0][n - 1]
 
 
@@ -49,10 +87,10 @@ if __name__ == '__main__':
     n = len(arr1)
     print(optimalStrategyOfGame(arr1, n))
 
-    arr2 = [2, 2, 2, 2]
-    n = len(arr2)
-    print(optimalStrategyOfGame(arr2, n))
+    # arr2 = [2, 2, 2, 2]
+    # n = len(arr2)
+    # print(optimalStrategyOfGame(arr2, n))
 
-    arr3 = [20, 30, 2, 2, 2, 10]
-    n = len(arr3)
-    print(optimalStrategyOfGame(arr3, n))
+    # arr3 = [20, 30, 2, 2, 2, 10]
+    # n = len(arr3)
+    # print(optimalStrategyOfGame(arr3, n))
