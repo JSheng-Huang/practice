e664b7bbdae621a5aca2dd7203a4bf2226e0b94d
jsheng <jasonhuang0124@gmail.com>
2023-11-23 09:00:12 +0800

    231123_0900: Keep going!

---
+++ dsa/misc/least_recently_used.py
@@ -0,0 +1,107 @@
+"""Least Recently Used
+Refer to:
+    1. https://tycoon0049.medium.com/%E6%BC%94%E7%AE%97%E6%B3%95%E7%B3%BB%E5%88%97-%E4%B8%80-lru-74b5771629ce
+Created by JSheng <jasonhuang0124@gmail.com>"""
+
+
+class Node:
+    def __init__(self, key: int, value: int, left=None, right=None):
+        self.prev = left
+        self.next = right
+        self.key = key
+        self.value = value
+
+
+class DoubleLinkedList:
+    def __init__(self):
+        self.head = Node(0, 0)
+        self.tail = Node(0, 0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        self.size = 0
+
+    def addLast(self, x: Node):
+        """Add the new node from the tail."""
+        x.next = self.tail
+        x.prev = self.tail.prev
+        self.tail.prev.next = x
+        self.tail.prev = x
+        self.size += 1
+
+    def remove(self, x: Node):
+        # 移除特定的節點
+        x.next.prev = x.prev
+        x.prev.next = x.next
+        x.prev = None
+        x.next = None
+        self.size -= 1
+
+    def removeFirst(self):
+        # 移除最久之前的節點
+        if self.head.next == self.tail:
+            return None
+        first_node = self.head.next
+        self.remove(first_node)
+        return first_node
+
+    def getSize(self):
+        return self.size
+
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        # 初始化
+        self.capacity = capacity
+        self.map = {}
+        self.lruCache = DoubleLinkedList()
+
+    def get(self, key: int) -> int:
+        x = self.map.get(key)
+        if x == None:
+            return -1
+        self.makeRecently(key)
+        return x.value
+
+    def put(self, key: int, value: int) -> None:
+        x = self.map.get(key)
+        if x != None:
+            self.deleteKey(key)
+            self.addRecently(key, value)
+            return
+        if self.capacity == self.lruCache.getSize():
+            self.removeLeastRecently()
+        self.addRecently(key, value)
+        return
+
+    def makeRecently(self, key: int):
+        # 將節點變成最新使用過的資料
+        x = self.map.get(key)
+        self.lruCache.remove(x)
+        self.lruCache.addLast(x)
+
+    def addRecently(self, key: int, value: int):
+        # 加入新節點並且是最新使用的資料
+        x = Node(key, value)
+        self.lruCache.addLast(x)
+        self.map[key] = x
+
+    def deleteKey(self, key: int):
+        # 刪除節點
+        x = self.map.get(key)
+        self.lruCache.remove(x)
+        self.map.pop(key)
+
+    def removeLeastRecently(self):
+        # 刪除最久沒使用的資料
+        firstNode = self.lruCache.removeFirst()
+        self.map.pop(firstNode.key)
+
+
+if __name__ == '__main__':
+    l_r_u = LRUCache(5)
+    l_r_u.put('q', 1)
+    l_r_u.put('w', 2)
+    l_r_u.put('e', 3)
+    l_r_u.put('a', 4)
+    l_r_u.put('s', 5)
+    print(l_r_u.get('a'))
