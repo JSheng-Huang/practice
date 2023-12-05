61318dc0b1384132217a71327b44dbfe14c38c87
jsheng <jasonhuang0124@gmail.com>
2023-12-05 12:04:05 +0800

    231205_1204: Keep going!

--- leetcode/misc/1362_medium_closest_divisors.py
+++ leetcode/misc/1362_medium_closest_divisors.py
@@ -21,9 +21,11 @@ Example:
       Input: num = 999
       Output: [40,25]
       Explanation: N/A.
-Refer to: ???
-Time Complexity: ???
-Space Complexity: ???
+Refer to: The following two authors use the same concept.
+  #1. https://leetcode.com/problems/closest-divisors/solutions/517962/greedy-beat-100-time-memory/
+  #2. https://leetcode.com/problems/closest-divisors/solutions/517595/java-c-python-easy-and-concise/
+    Time Complexity: O(sqrt(n))
+    Space Complexity: O(1)
 Created by JSheng <jasonhuang0124@gmail.com>"""
 
 # # For Function Annotations.
@@ -32,7 +34,18 @@ from typing import List
 
 class Solution:
     def closestDivisors(self, num: int) -> List[int]:
-        pass
+        """
+        Iterate candidates from int(sqrt(num+2)) to 1.
+        Check num + 1 before num + 2. Because when a candidate i(1 might the 
+        only one) is valid for both num + 1 and num + 2, The diff value of the 
+        former is smaller.
+        """
+        for i in range(int((num + 2) ** 0.5), 0, -1):
+            """`int()` would round down the parameter."""
+            if (num + 1) % i == 0:
+                return [i, (num + 1) // i]
+            if (num + 2) % i == 0:
+                return [i, (num + 2) // i]
 
 
 if __name__ == '__main__':
@@ -46,3 +59,9 @@ if __name__ == '__main__':
 
     """Return `[40, 25]`."""
     print(qwe.closestDivisors(999))
+
+    """Return `[3, 3]`."""
+    print(qwe.closestDivisors(7))
+
+    """Return `[2, 3]`."""
+    print(qwe.closestDivisors(4))
