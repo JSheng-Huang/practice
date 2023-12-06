44b4f9d67738442441134d4aa743bb0062385c74
jsheng <jasonhuang0124@gmail.com>
2023-12-06 12:58:46 +0800

    231206_1258: Keep going!

--- leetcode/misc/1716_easy_calculate_money in_leetcode_bank.py
+++ leetcode/misc/1716_easy_calculate_money in_leetcode_bank.py
@@ -27,17 +27,33 @@ Example:
             After the 20th day, the total is (1 + 2 + 3 + 4 + 5 + 6 + 7) + (2 + 
         3 + 4 + 5 + 6 + 7 + 8) + (3 + 4 + 5 + 6 + 7 + 8) = 96.
 Constraints: 1 <= n <= 1000
-Refer to: ???
-Time Complexity: ???
-Space Complexity: ???
+Refer to: Myself.
+Time Complexity: O(1)?
+Space Complexity: O(1)?
 Created by JSheng <jasonhuang0124@gmail.com>"""
 
 
 class Solution:
     def totalMoney(self, n: int) -> int:
-        
+        if n < 7:
+            if n == 1:
+                return 1
+            else:
+                return int((1 + n) * n / 2)
+        else:
+            week = n // 7
+            res = n % 7
+            multiplier = week * (week - 1) / 2
+            week_sum = 28 * week + 7 * multiplier
+            if res == 0:
+                return int(week_sum)
+            else:
+                res_bgn = 1 * week + 1
+                res_sum = (res_bgn + res_bgn + res - 1) * res / 2
+            return int(week_sum + res_sum)
 
 
 if __name__ == '__main__':
     qwe = Solution()
-    print(qwe.totalMoney(20))
+    """Return `135`."""
+    print(qwe.totalMoney(26))
