1172a744dbff4623e070dd4cb1b0a2325df7827a
jsheng <jasonhuang0124@gmail.com>
2023-12-11 09:00:41 +0800

    231211_0900: Keep going!

--- leetcode/misc/2076_hard_process_restricted_friend_requests.py
+++ leetcode/misc/2076_hard_process_restricted_friend_requests.py
@@ -77,6 +77,96 @@ class Solution:
             else:
                 blocked_map[i[0]] = []
                 blocked_map[i[0]].append(i[1])
+        for i in requests:
+            if blocked_map.get(i[0]):
+                if i[1] in blocked_map.get(i[0]) or i[1] in blocked_list:
+                    if i[1] not in blocked_list:
+                        blocked_list.append(i[1])
+                    res.append(False)
+            else:
+                res.append(True)
+        return res
+
+
+class Solution:
+    def friendRequests(self, n: int, restrictions: List[List[int]], requests: List[List[int]]) -> List[bool]:
+        # Maintain two lists of sets connected_components and banned_by_comps to store the connected components the restrictions of nodes in each connected component. Maintain a dictionary connected_comp_dict to map each node to its connected component. Update them when a new edge is added.
+        result = [False for _ in requests]
+
+        connected_components = [{i} for i in range(n)]
+
+        connected_comp_dict = {}
+        for i in range(n):
+            connected_comp_dict[i] = i
+        print(connected_comp_dict)
+        banned_by_comps = [set() for i in range(n)]
+        for res in restrictions:
+            banned_by_comps[res[0]].add(res[1])
+            banned_by_comps[res[1]].add(res[0])
+        print(banned_by_comps)
+        for i, r in enumerate(requests):
+            n1, n2 = r[0], r[1]
+            c1, c2 = connected_comp_dict[n1], connected_comp_dict[n2]
+            if c1 == c2:
+                result[i] = True
+            else:
+                if not connected_components[c1].intersection(banned_by_comps[c2]):
+                    connected_components[c1].update(connected_components[c2])
+                    banned_by_comps[c1].update(banned_by_comps[c2])
+                    for node in connected_components[c2]:
+                        connected_comp_dict[node] = c1
+                    result[i] = True
+
+        return result
+
+
+"""https://leetcode.com/problems/process-restricted-friend-requests/solutions/1576965/python3-union-find/"""
+
+
+class UnionFind:
+
+    def __init__(self, n: int):
+        self.parent = list(range(n))
+        self.rank = [1] * n
+
+    def find(self, p: int, halving: bool = True) -> int:
+        if p != self.parent[p]:
+            self.parent[p] = self.find(self.parent[p])
+        return self.parent[p]
+
+    def union(self, p: int, q: int) -> bool:
+        prt, qrt = self.find(p), self.find(q)
+        if prt == qrt:
+            return False
+        if self.rank[prt] > self.rank[qrt]:
+            prt, qrt = qrt, prt
+        self.parent[prt] = qrt
+        self.rank[qrt] += self.rank[prt]
+        return True
+
+
+class Solution:
+    def friendRequests(self, n: int, restrictions: List[List[int]], requests: List[List[int]]) -> List[bool]:
+        ans = []
+        uf = UnionFind(n)
+        print(uf.parent)
+        print(uf.rank)
+        for u, v in requests:
+            uu = uf.find(u)
+            vv = uf.find(v)
+            for x, y in restrictions:
+                xx = uf.find(x)
+                yy = uf.find(y)
+                if uu == xx and vv == yy or uu == yy and vv == xx:
+                    ans.append(False)
+                    break
+            else:
+                ans.append(True)
+        uf.union(u, v)
+        print(uf.parent)
+        print(uf.rank)
+
+        return ans
 
 
 if __name__ == '__main__':
