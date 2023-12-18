aec3c41f41bd699e08dd495c1561ec3453164c92
jsheng <jasonhuang0124@gmail.com>
2023-12-18 12:47:05 +0800

    231218_1247: Keep going!

--- leetcode/binary_search/114_flatten_binary_tree_to_linked_list_231218.py
+++ leetcode/binary_search/114_flatten_binary_tree_to_linked_list_231218.py
@@ -30,14 +30,52 @@ Date: 231218.
 Created by JSheng <jasonhuang0124@gmail.com>"""
 
 # # For Function Annotations.
-from typing import List, Optional, TreeNode
+from typing import List, Optional
 
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+# # For testing.
+
+
+class Node:
+    def __init__(self, value):
+        self.root = value
+        self.left = None
+        self.right = None
+
+
+# # For testing.
+def insert(node, value):
+    if node is None:
+        return Node(value)
+    else:
+        if node.root == value:
+            return node
+        elif not node.left:
+            node.left = Node(value)
+        elif not node.right:
+            node.right = Node(value)
+        else:
+            node.left = insert(node.left, value)
+    return node
+
+    # if node is None:
+    #     return Node(value)
+    # else:
+    #     if node.root == value:
+    #         return node
+    #     elif node.root < value:
+    #         node.right_child = insert(node.right_child, value)
+    #     else:
+    #         node.left_child = insert(node.left_child, value)
+    # return node
 
 
 class Solution:
@@ -45,24 +83,25 @@ class Solution:
         """
         Do not return anything, modify root in-place instead.
         """
-        # cur = root
-        # while cur:
-        #     if cur.left:
-        #         prev = cur.left
-        #         while prev.right:
-        #             prev = prev.right    # We go to left Subtree's rightMost Node
-        #         prev.right = cur.right  # We make current Node's right Subtree prev's right Subtree
-        #         cur.right = cur.left    # We make it right Subtree
-        #         cur.left = None   # Removing left
-        #     cur = cur.right
-        while n:
-            temp = n.left  # get left tree
-            if temp:
-                while temp.right:  # find its right most node
-                    temp = temp.right
-                # "cut" n.right and attach it to temp
-                temp.right, n.right, n.left = n.right, n.left, None
-            n = n.right
+        cur = root
+        while cur:
+            if cur.left:
+                prev = cur.left
+                while prev.right:
+                    prev = prev.right    # We go to left Subtree's rightMost Node
+                prev.right = cur.right  # We make current Node's right Subtree prev's right Subtree
+                cur.right = cur.left    # We make it right Subtree
+                cur.left = None   # Removing left
+            cur = cur.right
+
+        # while root:
+        #     temp = root.left  # get left tree
+        #     if temp:
+        #         while temp.right:  # find its right most node
+        #             temp = temp.right
+        #         # "cut" n.right and attach it to temp
+        #         temp.right, root.right, root.left = root.right, root.left, None
+        #     root = root.right
         """
         I think thats because we only every node once, Recursion also does that ryt.
         Only improvement is the time complexity.
@@ -78,3 +117,31 @@ class Solution:
 
 if __name__ == '__main__':
     qwe = Solution()
+
+    """
+        1
+       /   \
+      2     5
+      / \    \
+     3   4    6
+    """
+    root = Node(1)
+    root = insert(root, 2)
+    root = insert(root, 5)
+    root = insert(root, 3)
+    root = insert(root, 4)
+    root.right.right = Node(6)
+    print(root.root)
+    print(root.left.root)
+    print(root.right.root)
+    print(root.left.left.root)
+    print(root.left.right.root)
+    print(root.right.right.root)
+    print()
+    qwe.flatten(root)
+    print(root.root)
+    print(root.right.root)
+    print(root.right.right.root)
+    print(root.right.right.right.root)
+    print(root.right.right.right.right.root)
+    print(root.right.right.right.right.right.root)
