2ec561bbdd15feba52397396ff1ff24121f8cd15
jsheng <jasonhuang0124@gmail.com>
2023-12-19 09:00:19 +0800

    231219_0900: Keep going!

--- leetcode/misc/114_flatten_binary_tree_to_linked_list_231218.py
+++ leetcode/misc/114_flatten_binary_tree_to_linked_list_231218.py
@@ -22,9 +22,9 @@ Constraints:
     #1. The number of nodes in the tree is in the range [0, 2000].
     #2. -100 <= Node.val <= 100
 Follow up: Can you flatten the tree in-place (with O(1) extra space)?
-Refer to: https://leetcode.com/problems/flatten-binary-tree-to-linked-list/description/
-    Time Complexity: ???
-    Space Complexity: ???
+Refer to: https://leetcode.com/problems/flatten-binary-tree-to-linked-list/solutions/940394/python3-iterative-solution-with-explanation/
+    Time Complexity: N/A.
+    Space Complexity: O(1).
     Explanation: ???
 Date: 231218.
 Created by JSheng <jasonhuang0124@gmail.com>"""
@@ -32,18 +32,16 @@ Created by JSheng <jasonhuang0124@gmail.com>"""
 # # For Function Annotations.
 from typing import List, Optional
 
-# Definition for a binary tree node.
 
-
+# # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 
+
 # # For testing.
-
-
 class Node:
     def __init__(self, value):
         self.root = value
@@ -66,53 +64,27 @@ def insert(node, value):
             node.left = insert(node.left, value)
     return node
 
-    # if node is None:
-    #     return Node(value)
-    # else:
-    #     if node.root == value:
-    #         return node
-    #     elif node.root < value:
-    #         node.right_child = insert(node.right_child, value)
-    #     else:
-    #         node.left_child = insert(node.left_child, value)
-    # return node
-
 
 class Solution:
     def flatten(self, root: Optional[TreeNode]) -> None:
         """
         Do not return anything, modify root in-place instead.
-        """
-        cur = root
-        while cur:
-            if cur.left:
-                prev = cur.left
+        Written by LeetCode."""
+        while root:
+            if root.left:
+                prev = root.left
                 while prev.right:
-                    prev = prev.right    # We go to left Subtree's rightMost Node
-                prev.right = cur.right  # We make current Node's right Subtree prev's right Subtree
-                cur.right = cur.left    # We make it right Subtree
-                cur.left = None   # Removing left
-            cur = cur.right
+                    """We go to left Subtree's rightMost Node."""
+                    prev = prev.right
+                """We make current Node's right Subtree prev's right Subtree."""
+                prev.right = root.right
 
-        # while root:
-        #     temp = root.left  # get left tree
-        #     if temp:
-        #         while temp.right:  # find its right most node
-        #             temp = temp.right
-        #         # "cut" n.right and attach it to temp
-        #         temp.right, root.right, root.left = root.right, root.left, None
-        #     root = root.right
-        """
-        I think thats because we only every node once, Recursion also does that ryt.
-        Only improvement is the time complexity.
-        """
-        """
-        Only improvement is the time complexity.
-        Agree.
-        Since execution time is the same exactly - does it mean the difference due to recursion insignificant?
-        The test cases are relatively big.
-        Or it might be due to "server load" fluctuation?
-        """
+                """We make it right Subtree."""
+                root.right = root.left
+
+                """Removing left."""
+                root.left = None
+            root = root.right
 
 
 if __name__ == '__main__':
@@ -120,10 +92,10 @@ if __name__ == '__main__':
 
     """
         1
-       /   \
-      2     5
-      / \    \
-     3   4    6
+       /  \
+      2    5
+      / \   \
+     3   4   6
     """
     root = Node(1)
     root = insert(root, 2)
@@ -138,6 +110,20 @@ if __name__ == '__main__':
     print(root.left.right.root)
     print(root.right.right.root)
     print()
+
+    """
+    1
+      \
+       2
+        \
+         3
+          \
+           4
+            \
+             5
+              \
+               6
+    """
     qwe.flatten(root)
     print(root.root)
     print(root.right.root)
