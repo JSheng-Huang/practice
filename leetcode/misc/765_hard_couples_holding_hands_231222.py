7abaaff147451a74f7585c30a0ed45b9b6a1fde4
jsheng <jasonhuang0124@gmail.com>
2023-12-22 11:48:13 +0800

    231222_1148: Keep going!

--- leetcode/misc/765_hard_couples_holding_hands_231222.py
+++ leetcode/misc/765_hard_couples_holding_hands_231222.py
@@ -26,9 +26,9 @@ Constraints:
     #3. n is even.
     #4. 0 <= row[i] < 2n
     #5. All the elements of row are unique.
-Refer to: ???
-    Time Complexity: ???
-    Space Complexity: ???
+Refer to: https://leetcode.com/problems/couples-holding-hands/solutions/1258087/python3-a-few-approaches/
+    Time Complexity: O(n)?
+    Space Complexity: O(n)?
 Date: 231222.
 Created by JSheng <jasonhuang0124@gmail.com>"""
 
@@ -38,18 +38,45 @@ from typing import List, Optional
 
 class Solution:
     def minSwapsCouples(self, row: List[int]) -> int:
-        """row[n] and row[n + 1] should be couple"""
+        """Mine."""
         res = 0
         n = len(row)
         for i in range(0, n - 2, 2):
-            abs_diff = abs(row[i] - row[i + 1])
-            if abs_diff != 1:
+            """Trap
+            Can not just use +/- 1, because every even number only could pair with the odd number which it pluses 1.
+            """
+            if row[i] % 2 == 0:
+                target = row[i] + 1
+            else:
+                target = row[i] - 1
+            if row[i + 1] != target:
                 res += 1
                 for j in range(i + 2, n):
-                    abs_diff = abs(row[i] - row[j])
-                    if abs_diff == 1:
+                    if row[j] == target:
                         row[i + 1], row[j] = row[j], row[i + 1]
                         break
+        return res
+
+    def minSwapsCouples(self, row: List[int]) -> int:
+        """Modify from the reference."""
+        n = len(row)
+        loc_dict = {}
+        res = 0
+
+        """Use Hashmap to reduce the time complexity."""
+        for i in range(n):
+            loc_dict[row[i]] = i
+        for i in range(0, n - 2, 2):
+            if row[i] % 2 == 0:
+                target = row[i] + 1
+            else:
+                target = row[i] - 1
+            if row[i + 1] != target:
+                res += 1
+                target_idx = loc_dict[target]
+                loc_dict[row[i + 1]
+                         ], loc_dict[target] = loc_dict[target], loc_dict[row[i + 1]]
+                row[i + 1], row[target_idx] = row[target_idx], row[i + 1]
         return res
 
 
@@ -57,10 +84,13 @@ if __name__ == '__main__':
     qwe = Solution()
 
     """Should return `1`."""
-    # print(qwe.minSwapsCouples([0, 2, 1, 3]))
+    print(qwe.minSwapsCouples([0, 2, 1, 3]))
 
     """Should return `0`."""
-    # print(qwe.minSwapsCouples([3, 2, 0, 1]))
+    print(qwe.minSwapsCouples([3, 2, 0, 1]))
 
     """Should return `3`."""
     print(qwe.minSwapsCouples([6, 2, 1, 7, 4, 5, 3, 8, 0, 9]))
+
+    """Should return `1`."""
+    print(qwe.minSwapsCouples([5, 3, 4, 2, 1, 0]))
