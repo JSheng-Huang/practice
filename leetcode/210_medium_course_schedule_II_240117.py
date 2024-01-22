a1eef2cd5a254f4395e9ce19c20dfac4075642aa
jsheng <jasonhuang0124@gmail.com>
2024-01-22 16:50:03 +0800

    240122_1650: Keep going.

--- leetcode/210_medium_course_schedule_II_240117.py
+++ leetcode/210_medium_course_schedule_II_240117.py
@@ -36,7 +36,17 @@ Constraints:
     #4. 0 <= ai, bi < numCourses
     #5. ai != bi
     #6. All the pairs [ai, bi] are distinct.
-Refer to: 
+Refer to: https://leetcode.com/problems/course-schedule-ii/solutions/4284339/python-beginner-friendly-o-v-e/
+    Time Complexity: O(N + E).
+        Explanation:
+            `N` is the number of courses and `E` is the number of 
+        prerequisites. The DFS traversal visits each course once and each 
+        prerequisite once.
+    Space Complexity: O(N + E).
+        Explanation:
+            For the graph representation and recursion stack. `pre_req` 
+        dictionary stores the graph, and the visit and cycle sets store 
+        information during DFS.
 Date: 240117.
 Created by JSheng <jasonhuang0124@gmail.com>"""
 
@@ -47,37 +57,6 @@ from typing import List
 
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        """
-        https://leetcode.com/problems/course-schedule-ii/solutions/4284339/python-beginner-friendly-o-v-e/
-        """
-        """
-        Intuition
-        The problem is asking for the order in which courses can be taken given a list of prerequisites.
-        We can model the problem as a directed graph, where nodes represent courses and directed edges represent prerequisites.
-        The goal is to find a valid topological ordering of the graph.
-        Approach
-        We can use a depth-first search (DFS) based approach to find a valid topological ordering.
-
-        Create a dictionary (preReq) to represent the directed edges between courses and their prerequisites.
-        Initialize an empty list (res) to store the final result, a set (visit) to keep track of visited nodes, and a set (cycle) to detect cycles during the DFS traversal.
-        Define a DFS function that takes a course as an argument.
-        Check if the course is in the cycle set. If true, there is a cycle, and return False.
-        Check if the course is in the visit set. If true, the course is already processed, return True.
-        Add the course to the cycle set to mark it as visited.
-        Recursively call the DFS function for each prerequisite of the current course.
-        Remove the course from the cycle set (backtrack) and add it to the visit set.
-        Append the course to the result list.
-        Return True.
-        Iterate through all courses and call the DFS function for each course.
-        If the DFS function returns False, there is a cycle, and the result is not possible, return an empty list.
-        Return the result list as the valid topological ordering.
-        Complexity
-        Time complexity:
-        O(N + E), where N is the number of courses and E is the number of prerequisites. The DFS traversal visits each course once and each prerequisite once.
-
-        Space complexity:
-        O(N + E) for the graph representation and recursion stack. The preReq dictionary stores the graph, and the visit and cycle sets store information during DFS.
-        """
         pre_req = {}
         for i, j in prerequisites:
             if i not in pre_req:
@@ -91,12 +70,18 @@ class Solution:
         return res
 
     def dfs(self, c, pre_req, cycle, visit, res):
-        # terminating case
+        """Deadlock."""
         if c in cycle:
             return False
+        """The course has been checked."""
         if c in visit:
             return True
         cycle.add(c)
+
+        """
+        If the course `c` has prerequisites, check whether each of them would 
+        cause deadlock or not.
+        """
         if pre_req.get(c):
             for pr in pre_req[c]:
                 if self.dfs(pr, pre_req, cycle, visit, res) == False:
